0116f0abd20f273cafed785bbb717ac7d98dd0a3
KIM-MiN-0 <123252267+KIM-MiN-0@users.noreply.github.com>
2023-06-15 14:36:47 +0900

    Update Cpu0_Main.c
    양방향 통신가능하게 수정

--- UART_Receive/Cpu0_Main.c
+++ UART_Receive/Cpu0_Main.c
@@ -5,15 +5,49 @@
 #include "IfxScuWdt.h"
 #include "Ifx_Types.h"
 #include "IfxScuWdt.h"
+#include <stdio.h>
+
+
+//RGB_Register
+
+#define PORT2_BASE      (0xF003A200)
+#define PORT2_IOCR4     (*(volatile unsigned int*)(PORT2_BASE + 0x14))
+#define PORT2_OMR       (*(volatile unsigned int*)(PORT2_BASE + 0x04))
+
+#define PC7               27
+#define PCL7              23
+#define PS7               7
+
+
+#define PORT10_BASE     (0xF003B000)
+#define PORT10_OUT      (*(volatile unsigned int*)(PORT10_BASE))
+#define PORT10_OMSR     (*(volatile unsigned int*)(0xF003A670 + 0xA00))
+#define PORT10_OMCR     (*(volatile unsigned int*)(0xF003A680 + 0xA00))
+#define PORT10_IOCR0    (*(volatile unsigned int*)(PORT10_BASE + 0x10))
+#define PORT10_IOCR4    (*(volatile unsigned int*)(PORT10_BASE + 0x14))
+#define PORT10_OMR      (*(volatile unsigned int*)(PORT10_BASE + 0x04))
+
+#define PC5     11
+#define PC3     27
+
+#define PCL5    21
+#define PCL3    19
+#define PS5     5
+#define PS3     3
+
 
 #define IFX_INTPRIO_ASCLIN0_TX 1
 #define IFX_INTPRIO_ASCLIN0_RX 2
 #define IFX_INTPRIO_ASCLIN0_ER 3
 
+#define IFX_INTPRIO_ASCLIN1_TX 4
+#define IFX_INTPRIO_ASCLIN1_RX 5
+#define IFX_INTPRIO_ASCLIN1_ER 6
 
-#define IFX_INTPRIO_ASCLIN3_TX 4
-#define IFX_INTPRIO_ASCLIN3_RX 5
-#define IFX_INTPRIO_ASCLIN3_ER 6
+
+#define IFX_INTPRIO_ASCLIN3_TX 7
+#define IFX_INTPRIO_ASCLIN3_RX 8
+#define IFX_INTPRIO_ASCLIN3_ER 9
 
 #define PIN_LED13       &MODULE_P10,2
 #define PIN_LED12       &MODULE_P10,1
@@ -21,6 +55,7 @@
 IfxCpu_syncEvent g_cpuSyncEvent = 0;
 
 IfxAsclin_Asc asc;
+IfxAsclin_Asc asc1;
 IfxAsclin_Asc asclin_console;
 
 
@@ -40,6 +75,19 @@ IFX_INTERRUPT(asclin0ErISR, 0, IFX_INTPRIO_ASCLIN0_ER) {
 }
 
 
+
+IFX_INTERRUPT(asclin1TxISR, 0, IFX_INTPRIO_ASCLIN1_TX) {
+    IfxAsclin_Asc_isrTransmit(&asc1);
+}
+IFX_INTERRUPT(asclin1RxISR, 0, IFX_INTPRIO_ASCLIN1_RX) {
+    IfxAsclin_Asc_isrReceive(&asc1);
+}
+IFX_INTERRUPT(asclin1ErISR, 0, IFX_INTPRIO_ASCLIN1_ER) {
+    IfxAsclin_Asc_isrError(&asc1);
+}
+
+
+
 IFX_INTERRUPT(asclin3TxISR, 0, IFX_INTPRIO_ASCLIN3_TX) {
     IfxAsclin_Asc_isrTransmit(&asclin_console);
 }
@@ -49,6 +97,8 @@ IFX_INTERRUPT(asclin3RxISR, 0, IFX_INTPRIO_ASCLIN3_RX) {
 IFX_INTERRUPT(asclin3ErISR, 0, IFX_INTPRIO_ASCLIN3_ER) {
     IfxAsclin_Asc_isrError(&asclin_console);
 }
+void init_RGBLED(void);
+void initButton();
 
 void ASC0_init() {
     // create module config
@@ -87,6 +137,42 @@ void ASC0_init() {
     IfxAsclin_Asc_initModule(&asc, &ascConfig);
 }
 
+void ASC1_init() {
+    // create module config
+    IfxAsclin_Asc_Config ascConfig;
+
+    IfxAsclin_Asc_initModuleConfig(&ascConfig, &MODULE_ASCLIN1);
+
+    // set the desired baudrate
+    ascConfig.baudrate.prescaler = 1;
+    ascConfig.baudrate.baudrate = 9600;
+    ascConfig.baudrate.oversampling = IfxAsclin_OversamplingFactor_4;
+
+    // ISR priorities and interrupt target
+    ascConfig.interrupt.txPriority = IFX_INTPRIO_ASCLIN1_TX;
+    ascConfig.interrupt.rxPriority = IFX_INTPRIO_ASCLIN1_RX;
+    ascConfig.interrupt.erPriority = IFX_INTPRIO_ASCLIN1_ER;
+    ascConfig.interrupt.typeOfService = IfxSrc_Tos_cpu0;
+
+    // FIFO configuration
+    ascConfig.txBuffer     = &ascTxBuffer;
+    ascConfig.txBufferSize = ASC_TX_BUFFER_SIZE;
+    ascConfig.rxBuffer     = &ascRxBuffer;
+    ascConfig.rxBufferSize = ASC_RX_BUFFER_SIZE;
+    // pin configuration
+    const IfxAsclin_Asc_Pins pins
+        = {NULL,
+           IfxPort_InputMode_pullUp,        // CTS pin not used
+           &IfxAsclin1_RXA_P15_1_IN,        // RX IN (D0)
+           IfxPort_InputMode_pullUp,        // Rx pin
+           NULL,
+           IfxPort_OutputMode_pushPull,     // RTS pin not used
+           &IfxAsclin1_TX_P15_0_OUT,        // TX OUT (D1)
+           IfxPort_OutputMode_pushPull,     // Tx pin
+           IfxPort_PadDriver_cmosAutomotiveSpeed1};
+    ascConfig.pins = &pins;
+    IfxAsclin_Asc_initModule(&asc1, &ascConfig);
+}
 
 void ASC3_init() {
     // create module config
@@ -125,6 +211,27 @@ void ASC3_init() {
     IfxAsclin_Asc_initModule(&asclin_console, &ascConfig);
 }
 
+void init_RGBLED(void){
+
+
+    PORT2_IOCR4 &= ~((0x1F) << PC7);
+    PORT10_IOCR4 &= ~((0x1F) << PC5);
+    PORT10_IOCR0 &= ~((0x1F) << PC3);
+
+
+    PORT2_IOCR4 |= ((0x10) << PC7);
+    PORT10_IOCR4 |= ((0x10) << PC5);
+    PORT10_IOCR0 |= ((0x10) << PC3);
+
+}
+
+void initButton(){
+    // read switch input from P02.1
+    P02_IOCR0.U &= ~(0x1F<<11);//reset P02_IOCRO PC1
+    P02_IOCR0.U |= 0x03<<11 ;// set P02.1 general input (pull-up connected)
+
+
+}
 
 void delay(int cnt) {
     int cycle,j;
@@ -152,18 +259,29 @@ int core0_main(void) {
     IfxPort_setPinLow(PIN_LED13);
 
     ASC0_init();
+    ASC1_init();
     ASC3_init();
 
+    init_RGBLED();
+    initButton();
+
+
     uint16 duty;
+    uint8 Button;
+
+    Ifx_SizeT send_len = 1;
     Ifx_SizeT receive_len = 2;
 
     char text[10];
     Ifx_SizeT len = 7;
 
     while(1) {
-        IfxAsclin_Asc_read(&asc, &duty, &receive_len, TIME_INFINITE);
 
-        text[4] = duty % 10 + '0';
+
+        IfxAsclin_Asc_read(&asc, &duty, &receive_len, TIME_INFINITE);
+        IfxAsclin_Asc_write(&asc1, &Button, &send_len, TIME_INFINITE);
+
+       text[4] = duty % 10 + '0';
         text[3] = (duty/10) % 10 + '0';
         text[2] = (duty/100) % 10 + '0';
         text[1] = (duty/1000) % 10 + '0';
@@ -174,19 +292,46 @@ int core0_main(void) {
 
         IfxAsclin_Asc_write(&asclin_console, &text, &len, TIME_INFINITE);
 
-        if (duty < 30) {
-            IfxPort_setPinLow(PIN_LED12);
-            IfxPort_setPinHigh(PIN_LED13);
-        } else if (duty > 30) {
-            IfxPort_setPinHigh(PIN_LED12);
-            IfxPort_setPinHigh(PIN_LED13);
-        }else if (duty < 80) {
-            IfxPort_setPinHigh(PIN_LED12);
-            IfxPort_setPinLow(PIN_LED13);
-        }else if (duty > 80) {
-            IfxPort_setPinLow(PIN_LED12);
-            IfxPort_setPinLow(PIN_LED13);
-        }
+                      if(duty >= 10000)
+                       {
+                           PORT2_OMR |= (1<<PS7);            // Set LED RED
+                           PORT10_OMR |= (1<<PCL5);           // Clear LED GREEN
+                           PORT10_OMR |= (1<<PCL3);           // Clear LED BLUE
+                       }
+                       else if(duty >= 7000)
+                       {
+                           PORT2_OMR |= (1<<PCL7);            // Clear LED RED
+                           PORT10_OMR |= (1<<PS5);             // Set LED GREEN
+                           PORT10_OMR |= (1<<PCL3);            // Clear LED BLUE
+                       }
+                       else if(duty >= 3000)
+                       {
+                           PORT2_OMR |= (1<<PCL7);            // Clear LED RED
+                           PORT10_OMR |= (1<<PCL5);            // Clear LED GREEN
+                           PORT10_OMR |= (1<<PS3);             // Set LED BLUE
+                       }
+                       else
+                       {
+                           PORT2_OMR |= (1<<PCL7);            // Clear LED RED
+                           PORT10_OMR |= (1<<PCL5);            // Clear LED GREEN
+                           PORT10_OMR |= (1<<PCL3);            // Clear LED BLUE
+                       }
+
+
+                      if((P02_IN.U & (0x01)) == 0)   //여기를 무조건 0으로 쓰자...
+                              {//Button Pressed
+                                  P10_OUT.U |= 0x1 << 2; // Blue LED ON
+                                  Button = 1;
+
+                              }
+                              else{
+                                  //Button released.
+                                  P10_OUT.U &= ~(0x1 << 2); // Blue LED OFF
+                                  Button = 0;
+
+                              }
+
+
     }
 
     return 1;
